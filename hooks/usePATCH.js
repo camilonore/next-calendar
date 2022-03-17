9df7210d9490645e990f37b6e184ddaf13c06f35
Camilo292 <caminore22@gmail.com>
2022-03-17 11:38:46 -0500

    Update

--- hooks/usePATCH.js
+++ hooks/usePATCH.js
@@ -9,7 +9,6 @@ const usePATCH = async ({ event, fields, isActivity, editObjectId }) => {
     body[field.name] = event.target[field.name].value
   })
 
-  console.log(body)
   const response = await patchData(url, body)
   fields.forEach(field => {
     event.target[field.name].value = ''
